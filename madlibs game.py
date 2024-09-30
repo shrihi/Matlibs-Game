ad1ddf5fb5b78c00c98661133d687574377bd3a4
Hitashri M <144334872+shrihi@users.noreply.github.com>
2024-09-30 18:03:53 +0530

    Add files via upload

---
+++ madlibs game.py
@@ -0,0 +1,37 @@
+#Madlibs Game
+#word game where you create a story by filling in random words
+
+
+#Once upon a time, a [adjective] explorer [verb ending in -ing] through a [adjective] forest. 
+#They discovered a [noun] that was [adjective] and decided to take it home.
+#Little did they know, it would change their life in a [adjective] way!
+
+
+
+print("Please refer to the interesting paragraph given below.")
+print("Fill in the blancks accordingly.")
+print("Once upon a time, a [____1____] explorer [____2____] through a [____3____] forest. They discovered a [____4____] that was [____5____] and decided to take it home. Little did they know, it would change their life in a [____6____] way!")
+
+
+
+adj1_1=input("1 - Enter an adjective here which are describing words!")
+verb_2=input("2 - Enter a verb ending in -ing here which are action words!")
+adj2_3=input("3 - Enter an adjective here which are describing words!")
+noun_4=input("4 - Enter the noun here which are Naming words")
+adj3_5=input("5 - Enter an adjective here which are describing words!")
+adj4_6=input("6 - Enter an adjective here which are describing words!")
+
+
+print(f"Once upon a time, a {adj1_1} explorer {verb_2} through a {adj2_3} forest. They discovered a {noun_4} that was {adj3_5} and decided to take it home. Little did they know, it would change their life in a {adj4_6} way!")
+
+
+
+#Sample Output - Once upon a time, a brave explorer was trekking through a mysterious forest. They discovered a treasure that was glowing and decided to take it home. Little did they know, it would change their life in a magical way!
+'''
+brave (adjective)
+trekking (verb)
+mysterious (adjective)
+treasure (noun)
+glowing (adjective)
+magical (adjective)
+'''
